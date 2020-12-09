3ec6ae909be94ab52eb795614cd73b0cb2296a17
raghavendra.kotipall <Raghavendra.kotipalli@ggktech.com>
2020-12-09 23:02:24 +0530

    first commit

---
+++ PHPmailer/PHPmailer/mail.php
@@ -0,0 +1,53 @@
+    
+<?php
+require "PHPMailerAutoload.php";
+function smtpmailer($to, $from, $from_name, $subject, $body)
+    {
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true; 
+ 
+        $mail->SMTPSecure = 'ssl'; 
+        $mail->Host = 'instaglamexpress.com';
+        $mail->Port = 465;  
+        $mail->Username = 'info@instaglamexpress.com';
+        $mail->Password = 'insta@007711';   
+        $mail->IsHTML(true);
+        $mail->From="info@instaglamexpress.com";
+        $mail->FromName=$from_name;
+        $mail->Sender=$from;
+        $mail->AddReplyTo($from, $from_name);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AddAddress($to);
+        if(!$mail->Send())
+        {
+            $error ="Please try Later, Error Occured while Processing...";
+            return $error; 
+        }
+        else 
+        {
+            $error = "Thanks You !! Your email is sent.";  
+            return $error;
+        }
+    }
+    
+    $to   = 'surendrapinninti5@gmail.com';
+    $from = 'info@instaglamexpress.com'; 
+    $name = 'INSTAGLAM EXPRESS';
+    $subj = 'PHPMailer 5.2 testing from DomainRacer';
+    $msg = 'This is mail about testing mailing using PHP.';
+    
+    $error=smtpmailer($to,$from, $name ,$subj, $msg);
+    
+?>
+
+<html>
+    <head>
+        <title>PHPMailer 5.2 testing from DomainRacer</title>
+    </head>
+    <body style="background: black;">
+        <center><h2 style="padding-top:70px;color: white;"><?php echo $error; ?></h2></center>
+    </body>
+    
+</html>
